3f37dc09ede1e7bef008d2bbe1cf1a16b83df49f
Stephen Saunders <saunders.stephen@gmail.com>
2016-02-24 16:26:01 -0800

    modify webpack build process with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,17 @@
 var path = require('path');
 
 module.exports = {
-    entry: {
-      app: path.join(__dirname, './src/client/app.js')
-    },
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "[name].bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-        ]
-    }
+  entry: {
+    app: path.join(__dirname, './src/client/app.js')
+  },
+  output: {
+      path: path.join(__dirname, "public"),
+      filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  }
 };
