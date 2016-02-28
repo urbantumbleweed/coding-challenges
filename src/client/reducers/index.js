52ee761c28610cec60e211fd890e6219c2dfa409
Stephen Saunders <saunders.stephen@gmail.com>
2016-02-27 17:03:05 -0800

    add psuedocode scaffolding for root reducer and truck reducer

---
+++ src/client/reducers/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import { combineReducers } from 'redux';
+// import trucks from './trucks'
+// import activeTruck from './activeTruck'
+// import user from './user'
+// import view from './view'
+// import search from './search'
+// import search from './map'
+
+const rootReducer = combineReducers({
+  //trucks
+  //activeTruck
+  //user
+  //view
+  //search
+  //map
+});
+
+export default rootReducer;
---
+++ src/client/reducers/trucks.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//get constants to use for switch statement
+
+// set the initial state
+
+
+// export the truck reducer function
+  // which accepts state and payload
+    // state should default to initial state
+  // switch statement on payload.type
+  // match on imported constants
